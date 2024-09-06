cdcf413215eaff02204fcaddbcdd38afb9898183
jacobwaldman <jacobwdev@gmail.com>
2024-09-06 19:06:40 -0400

    updated javascript for click events to interact with drum machine

--- app.js
+++ app.js
@@ -3,7 +3,15 @@
 const drumKeys = document.querySelectorAll(".drum-key");
 
 drumKeys.forEach(function (key) {
-  key.addEventListener("click", function (e) {
-    e.target.closest(".drum-key").classList.add("playing");
+  key.addEventListener("mousedown", function (e) {
+    e.target.closest(".drum-key").classList.toggle("playing");
+  });
+
+  key.addEventListener("mouseup", function (e) {
+    e.target.closest(".drum-key").classList.remove("playing");
   });
 });
+
+// window.addEventListener("keydown", function (e) {
+//   console.log(e);
+// });
