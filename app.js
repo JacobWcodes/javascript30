9994413c7529d7410f5e974bf77b4c9cec133794
jacobwaldman <jacobwdev@gmail.com>
2024-09-09 17:17:31 -0400

    updated html, css, and JS to complete the project for now

--- app.js
+++ app.js
@@ -2,16 +2,30 @@
 
 const drumKeys = document.querySelectorAll(".drum-key");
 
-drumKeys.forEach(function (key) {
-  key.addEventListener("mousedown", function (e) {
-    e.target.closest(".drum-key").classList.toggle("playing");
-  });
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(
+    `audio[data-key="${e.key}"]`.toLowerCase()
+  );
+  const key = document.querySelector(
+    `.drum-key[data-key="${e.key}"]`.toLowerCase()
+  );
+  // const lowerOpen =
+  const openHat = document.querySelector(`audio[data-key="f"]`.toLowerCase());
 
-  key.addEventListener("mouseup", function (e) {
+  if (!audio) return;
+
+  // Stop open hat from playing when closed hat is played:
+  if (e.key.toLowerCase() === "s") {
+    openHat.pause();
+  }
+
+  key.classList.add("playing");
+  audio.currentTime = 0;
+  audio.play();
+});
+
+drumKeys.forEach(function (key) {
+  key.addEventListener("transitionend", function (e) {
     e.target.closest(".drum-key").classList.remove("playing");
   });
 });
-
-// window.addEventListener("keydown", function (e) {
-//   console.log(e);
-// });
