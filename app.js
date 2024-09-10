2569f56b6de8de7dc787a28f7284fe0dee0eabb6
jacobwaldman <jacobwdev@gmail.com>
2024-09-10 16:31:10 -0400

    first commit of clock project

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const hourHand = document.querySelector(".hour");
+const minsHand = document.querySelector(".minute");
+const secondHand = document.querySelector(".second");
+
+const time = function () {
+  const timeNow = new Date();
+  const second = timeNow.getSeconds();
+  const minute = timeNow.getMinutes();
+  const hour = timeNow.getHours();
+  const secondDegrees = (second / 60) * 360 + 90;
+  const minuteDegrees = (minute / 60) * 360 + 90;
+  const hourDegrees = (hour / 12) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+  minsHand.style.transform = `rotate(${minuteDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  //   console.log(`${hour}:${minute}:${second}`);
+};
+
+time();
+
+setInterval(time, 1000);
