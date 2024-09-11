dd586e5d360699e62f825e6b3ee6c37bfb08e118
jacobwaldman <jacobwdev@gmail.com>
2024-09-11 18:33:26 -0400

    Updates to CSS and JS

--- 05 Flex Panels Image Gallery/app.js
+++ 05 Flex Panels Image Gallery/app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", open);
+  panel.addEventListener("transitionend", active);
+});
+
+function open() {
+  this.classList.toggle("open");
+}
+
+function active(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
