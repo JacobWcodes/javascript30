16eb9837e342ca132472cd306991af03e51dfb4e
jacobwaldman <jacobwdev@gmail.com>
2024-09-12 13:45:35 -0400

    Updated JS logic so panels open/close properly

--- 05 Flex Panels Image Gallery/app.js
+++ 05 Flex Panels Image Gallery/app.js
@@ -2,17 +2,20 @@
 
 const panels = document.querySelectorAll(".panel");
 
-panels.forEach((panel) => {
-  panel.addEventListener("click", open);
-  panel.addEventListener("transitionend", active);
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function (e) {
+    // Check if any panel is open when clicking another panel and close the open panels:
+    let clicked = e.target.closest(".panel"); // grabbing the clicked panel
+    // Closing all the panels that are clicked, but leaving the clicked panel untouched:
+    panels.forEach(function (p) {
+      if (p !== clicked) {
+        p.classList.remove("open");
+        p.classList.remove("open-active");
+      }
+    });
+
+    // Toggling panel that was clicked:
+    panel.classList.toggle("open");
+    panel.classList.toggle("open-active");
+  });
 });
-
-function open() {
-  this.classList.toggle("open");
-}
-
-function active(e) {
-  if (e.propertyName.includes("flex")) {
-    this.classList.toggle("open-active");
-  }
-}
