a15cf9975430e74f78f91aceede6f601a1a30b33
jacobwaldman <jacobwdev@gmail.com>
2024-09-17 17:51:43 -0400

    First commit

---
+++ 06 Ajax Type Ahead/app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+// 1. Get data and push to array:
+const cities = [];
+
+fetch(endpoint).then((dataBulk) =>
+  dataBulk.json().then((data) => cities.push(...data))
+);
+
+// 1. filter through cities array based on what's searched:
+function searchCities(value, cities) {
+  // NOTE - Make sure to use 'return' keyword so that filtered data can be used!!!!!!!!!!!!!!!
+  return cities.filter((place) => {
+    // making sure everything is lower case
+    const lowerCity = place.city.toLowerCase();
+    const lowerState = place.state.toLowerCase();
+    // NOTE - Make sure to use 'return' keyword so that filtered data can be used!!!!!!!!!!!!!!!
+    return lowerCity.match(value) || lowerState.match(value);
+  });
+}
+// ----------------------------
+// Alternative to above filter by using regular expressions:
+// function searchCities(value, cities) {
+//   // NOTE - Make sure to use 'return' keyword so that filtered data can be used!!!!!!!!!!!!!!!
+//   return cities.filter((place) => {
+//     const regex = new RegExp(value, "gi"); // 'g' is for 'global' search. 'i' is for case insensitivity.
+//     // NOTE - Make sure to use 'return' keyword so that filtered data can be used!!!!!!!!!!!!!!!
+//     return place.city.match(regex) || place.state.match(regex);
+//   });
+// }
+// ---------------------------
+
+// 2. Whatever is returned after filter, loop through and display each separately in li as a list under the search bar ;
+function displaySearch() {
+  const matchedArray = searchCities(this.value, cities);
+  matchedArray.map((place) => {});
+}
+
+const searchInput = document.querySelector(".search");
+
+const ul = document.querySelector(".item-container");
+
+searchInput.addEventListener("keyup", displaySearch);
